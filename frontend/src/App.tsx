7235eb9186e11d34c8398027a51b87559b6ad862
Neddys0404 <tepxcube@gmail.com>
2026-09-07 01:59:34 +0800

    adjustment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -471,7 +471,7 @@ function NodeCard({ id, data, type, selected }: NodeProps<W> & { type: string })
                         id={input.name}
                         style={{
                           ...HANDLE_STYLE,
-                          left: -1,
+                          left: -15,
                           top: "50%",
                           transform: "translateY(-50%)",
                         }}
